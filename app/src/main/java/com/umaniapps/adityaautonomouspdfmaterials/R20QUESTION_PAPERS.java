acaba46afede762a60bca4af99bf64b0ae61aff5
umaniapps <umaniplaystore@gmail.com>
2022-04-26 20:50:26 +0530

    super

---
+++ app/src/main/java/com/umaniapps/adityaautonomouspdfmaterials/R20QUESTION_PAPERS.java
@@ -0,0 +1,14 @@
+package com.umaniapps.adityaautonomouspdfmaterials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class R20QUESTION_PAPERS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_r20_question_papers);
+    }
+}
